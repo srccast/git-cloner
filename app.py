6bb8b57e4df6a9bd6b5b7adbc3628046bf603a88
Fabian Lange <fabian.lange@srccast.de>
2024-05-26 12:49:36 +0200

    Add app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import io
+import logging
+import tarfile
+from tempfile import TemporaryDirectory
+
+from flask import Flask, send_file
+import docker
+
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+
+log = logging.getLogger(__name__)
+
+REPO = "https://github.com/srccast/django_management_command_decorator.git"
+
+
+@app.route("/")
+def hello_world():
+    client = docker.DockerClient.from_env()
+
+    with TemporaryDirectory(dir="/data") as temp_dir:
+        app.logger.info(f"Cloning {REPO} into {temp_dir}")
+
+        client.containers.run(
+            "alpine/git",
+            f"clone {REPO} {temp_dir}",
+            remove=True,
+            volumes=[
+                "/data:/data"
+            ]
+        )
+
+        out_f = io.BytesIO()
+        with tarfile.open(fileobj=out_f, mode='w') as f:
+            f.add(temp_dir, recursive=True)
+
+        out_f.seek(0)
+        return send_file(path_or_file=out_f, mimetype="application/x-tar")
